9e3c0766f3959a146df8b7e95e50cd97be82a131
werpuc <puchala.weronika@gmail.com>
2018-11-27 17:55:50 +0100

    collecting data

---
+++ generate_weather_data.py
@@ -0,0 +1,59 @@
+from bs4 import BeautifulSoup
+import urllib3
+import re
+import os
+
+
+def scrap_weather_data_into_file(month, year, place):
+
+	"""
+	Download json with historical weather data per month and year in place from timeanddate.com.
+	Program creates subfolder called data_files if not exisits and populates it with data files.
+	"""
+
+	try:
+		month = int(month)
+	except ValueError:
+		print('month not valid, cannot download data')
+		return
+
+	try:
+		year = int(year)
+	except ValueError:
+		print('year not valid, cannot download data')
+		return
+
+	url = 'https://www.timeanddate.com/weather/poland/'+place+'/historic?month='+str(month)+'&year='+str(year)
+
+	http = urllib3.PoolManager()
+	r = http.request('GET', url)
+	soup = BeautifulSoup(r.data, 'html.parser')
+	element = soup.find(string=re.compile("var data"))
+
+	directory = 'data_files'
+	file_name = directory+'/'+place+'-'+str(year)+('0'+str(month) if month <10 else str(month))+'.json'
+
+	if not os.path.exists(directory):
+		try:
+			os.makedirs(directory)
+		except:
+			print('something wrong with data files management')
+			return
+
+	with open(file_name, 'w+') as file:
+		file.write(element[10:-34])
+
+	print("Data in json downloaded and written down in : " + file_name)
+
+
+def main():
+
+	scrap_weather_data_into_file(8, 2018, 'krakow')
+	scrap_weather_data_into_file(9, 2018, 'krakow')
+	scrap_weather_data_into_file(10, 2018, 'krakow')
+	scrap_weather_data_into_file(11, 2018, 'krakow')
+
+
+if __name__ == '__main__':
+	main()
+	
---
+++ weather_json_to_csv.R
@@ -0,0 +1,58 @@
+library(rjson)
+library(data.table)
+library(tidyverse)
+library(anytime)
+
+setwd('C:\\Users\\pucha\\Projects\\circle_of_life')
+
+## function to read json into table
+
+get_data_from_json_into_df <- function(file_name, data){
+  
+  result <- fromJSON(file = file_name)
+  all <- as.data.frame(do.call("rbind", result['detail']))
+  end <- 4 * (length(all) %/% 4)
+  
+  for (i in c(1:end)){
+    tmp <- as.data.frame(all[i][[1]]$detail)
+    data <- bind_rows(data, tmp)
+  } 
+  
+  return(data)
+}
+
+## creating table from all data files
+
+items <- list.files("data_files", pattern='*.json')
+data <- data.frame()
+
+for (i in 1:length(items)){
+  data <- get_data_from_json_into_df(paste('data_files\\',items[i], sep=''), data)
+}
+
+## manipulating data
+
+data <- data %>%
+  select(-icon, -ds, -hl, -hls, -hlsh, -wd) %>%
+  mutate(ts = parse_time(ts)) %>%
+  mutate(is_cloudy = grepl('cloud', desc),
+         is_sunny = grepl('sun', desc),
+         is_rainy = grepl('rain', desc),
+         is_snowy = grepl('snow', desc),
+         is_foggy = grepl('fog', desc),
+         date = anydate(date/1000)) %>%
+  select(-desc)
+
+# # common weather features in desc to analyse logical values
+# data %>%
+#   select(desc) %>%
+#   group_by(desc) %>%
+#   count() %>%
+#   arrange(desc(n))
+
+## write to file for later use
+
+write_csv(data, 'weather_data_cumulated.csv')
+
+
+
