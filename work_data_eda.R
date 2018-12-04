6bc9a6dc212d03c5fed27163ee9fe59b5e8c6d57
werpuc <puchala.weronika@gmail.com>
2018-12-04 23:44:07 +0100

    adds some plots

--- work_data_eda.R
+++ work_data_eda.R
@@ -1,6 +1,7 @@
 library(skimr)
 library(tidyverse)
 library(wesanderson)
+library(DT)
 
 # work_data check
 
@@ -35,6 +36,7 @@ work_data %>%
     #                       breaks=c("min", "mean", "max"),
     #                       labels=c("min", "mean", "max"))
 
+# pomodoros per month
 work_data %>%
   filter(wday(date)!=1 & wday(date)!=7) %>%
   group_by(month = month(date, label = TRUE)) %>%
@@ -43,5 +45,47 @@ work_data %>%
   ggplot(aes(x = month, y = monthly_pomodoroes)) +
     geom_col(fill = colores[1]) +
     geom_hline(yintercept = 160, linetype="dashed", color = colores[2]) + 
-    scale_fill_manual(values = colores)
+    geom_text(aes(3, 160, label = '80 hours', vjust = -.5, color = colores[2])) + 
+    scale_fill_manual(values = colores) +
+    theme(legend.position="none")
+
+# pomodoros per week
+
+avg_pomodoros_weekly <-  work_data %>%
+  group_by(week_no = week(date)) %>%
+  summarize(weekly_pomodores = sum(pomodoros, na.rm = TRUE)) %>%
+  summarize(mean(weekly_pomodores)) %>%
+  .[[1]] %>%
+  round()
+
+work_data %>%
+  group_by(week_no = week(date)) %>%
+  summarize(weekly_pomodoroes = sum(pomodoros, na.rm = TRUE),
+            avg_weekly_pomodores = round(mean(pomodoros, na.rm = TRUE), 2)) %>%
+  mutate(hours = weekly_pomodoroes/2) %>%
+  ggplot(aes(x = week_no, y = weekly_pomodoroes)) +
+  geom_col(fill = colores[1]) +
+  geom_hline(yintercept = 40, linetype="dashed", color = colores[2]) + 
+  geom_text(aes(45, 40, label = '20 hours', vjust = -.5, color = colores[2])) + 
+  scale_fill_manual(values = colores) +
+  geom_hline(yintercept = avg_pomodoros_ever, linetype = "dashed", color = colores[3]) + 
+  geom_text(aes(45, avg_pomodoros_weekly, label = paste0(avg_pomodoros_weekly/2, ' hours'), vjust = -.5)) + 
+  geom_text(aes(45, avg_pomodoros_weekly, label = 'average', vjust = 1.2)) + 
+  theme(legend.position="none") + 
+  labs(x = 'week', y = 'sum of pomodoros', title = 'Sum of pomodoros per week') 
+
+# pomodors per day
+
+avg_pomodoros_daily = round(mean(work_data[wday(work_data$date)!=1 & wday(work_data$date)!=7, ]$pomodoros, na.rm = TRUE))
+
+work_data %>%
+  mutate(is_workday = (wday(date)!=1 & wday(date)!=7)) %>%
+  ggplot(aes(x = date, y = pomodoros)) +
+    geom_col(aes(fill = is_workday), show.legend = TRUE, width = 1) +
+    scale_fill_manual(values = colores) +
+    geom_hline(yintercept = avg_pomodoros_daily, linetype = 'dashed', color = colores[3]) + 
+    geom_text(aes(as.Date('2018-11-09'), y = avg_pomodoros_daily, label = paste0(avg_pomodoros_daily/2, ' hours'), vjust = -.5)) +
+    geom_text(aes(as.Date('2018-11-09'), y = avg_pomodoros_daily, label = 'average', vjust = 1.3)) +
+    labs(x = 'month', y = 'pomodoros', title = 'Pomodoros in time')
+
 
